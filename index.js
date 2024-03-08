dce32940d909317a9915d437d8d145eb1939e086
Koby <kobysac@gmail.com>
2024-03-07 23:36:41 -0800

    can fetch a url

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+const fetch = require('node-fetch');
 
+
+function checkUrl() {
+  if(process.argv.length < 3) {
+    console.log('A url is required, but was not provided.')
+    return false;
+  }
+  console.log(process.argv[2]);
+  return process.argv[2];
+}
 
 async function main() {
-  console.log("Hello World!");
+  const url = checkUrl();
+  // if (!url) return;
+
+  try {
+    console.log(`Retrieving resource from ${url}`);
+    const response = await fetch('http://youtube.com');
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    console.log('Make sure the provided url is valid.')
+  }
+
 }
 
 
